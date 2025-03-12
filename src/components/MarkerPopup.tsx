312689f384ebaa8b5920babea3c94de00d74eadc
유은철 <isz06188@iansit.co.kr>
2025-03-12 18:01:01 +0900

    아이콘, 타이틀 설정,

--- src/components/MarkerPopup.tsx
+++ src/components/MarkerPopup.tsx
@@ -16,7 +16,6 @@ const MarkerPopup: React.FC<MarkerPopupProps> = ({ hospital }) => {
       <h3 className="hospital-name">{hospital.dutyName}</h3>
       <div className="hospital-info">
         <p className="hospital-type">{hospital.dutyEmclsName}</p>
-        <p className="hospital-address">{hospital.dutyAddr}</p>
         <p className="hospital-tel">{hospital.dutyTel1}</p>
       </div>
     </div>
